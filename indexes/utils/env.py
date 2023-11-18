ec23dec3ff7851570fef4d8a23d74f6b61e6e65b
Vladimir <vi142696@gmail.com>
2023-11-18 02:14:10 +0100

    init: init

---
+++ indexes/utils/env.py
@@ -0,0 +1,12 @@
+import os
+
+
+CLUSTER = os.environ['CLUSTER']
+DATA_GENERATION = os.environ['DATA_GENERATION']
+
+REDIS_HOST = os.environ['REDIS_HOST']
+REDIS_PORT = os.environ['REDIS_PORT']
+REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+
+SERVICE_NAME = os.environ['SERVICE_NAME']
+SERVICE_PORT = os.environ['SERVICE_PORT']
