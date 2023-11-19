fb81ad617bf72601dc6b8c3f7bf5dcdff2557450
Vladimir <vi142696@gmail.com>
2023-11-19 12:19:46 +0100

    refactor: removed useless variables

--- gateway/utils/env.py
+++ gateway/utils/env.py
@@ -4,6 +4,3 @@ import os
 REDIS_HOST = os.environ['REDIS_HOST']
 REDIS_PORT = os.environ['REDIS_PORT']
 REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
-
-SERVICE_NAME = os.environ['SERVICE_NAME']
-SERVICE_PORT = os.environ['SERVICE_PORT']
